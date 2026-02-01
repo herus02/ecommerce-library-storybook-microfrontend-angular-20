8170ac27827f11f695692196f537510605af5d3e
Herus Armstrong <herus02@gmail.com>
2026-02-01 18:05:50 -0300

    Fix shell dependencies

--- apps/shell/src/app/nx-welcome.ts
+++ apps/shell/src/app/nx-welcome.ts
@@ -820,11 +820,7 @@ import { CommonModule } from '@angular/common';
               </div>
               <p>Your Nx Cloud remote cache setup is almost complete.</p>
 
-              <a
-                href="https://cloud.nx.app/connect/rtc63WLjCv"
-                target="_blank"
-                rel="noreferrer"
-              >
+              <a href="" target="_blank" rel="noreferrer">
                 Click here to finish
               </a>
             </div>
--- apps/shell/src/main.server.ts
+++ apps/shell/src/main.server.ts
@@ -1,11 +1,65 @@
-import {
-  BootstrapContext,
-  bootstrapApplication,
-} from '@angular/platform-browser';
-import { App } from './app/app';
-import { config } from './app/app.config.server';
+import { APP_BASE_HREF } from '@angular/common';
+import { CommonEngine } from '@angular/ssr/node';
+import express from 'express';
+import cors from 'cors';
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
+import bootstrap from './bootstrap.server';
 
-const bootstrap = (context: BootstrapContext) =>
-  bootstrapApplication(App, config, context);
+// The Express app is exported so that it can be used by serverless Functions.
+export function app(): express.Express {
+  const server = express();
+  const distFolder = join(process.cwd(), 'dist/apps/shell/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? join(distFolder, 'index.original.html')
+    : join(distFolder, 'index.html');
+
+  server.use(cors());
+
+  const commonEngine = new CommonEngine();
+
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
+
+  // Example Express Rest API endpoints
+  // server.get('/api/**', (req, res) => { });
+  // Serve static files from /browser
+  server.get(
+    '*.*',
+    express.static(distFolder, {
+      maxAge: '1y',
+    }),
+  );
+
+  // All regular routes use the Angular engine
+  server.get('*', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
+
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: distFolder,
+        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+      })
+      .then((html) => res.send(html))
+      .catch((err) => next(err));
+  });
+
+  return server;
+}
+
+function run(): void {
+  const port = process.env['PORT'] || 4000;
+
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
+
+run();
 
 export default bootstrap;
